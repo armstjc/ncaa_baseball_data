18ae851e9d7c64e25feea54e7bef83abb19b1d7d
Joseph Armstrong <70788681+armstjc@users.noreply.github.com>
2026-02-20 01:02:54 -0500

    Data Update

--- main.sh
+++ main.sh
@@ -1,7 +1,8 @@
-# current_date = $(date)
+current_date = $(date)
 
 git pull
 python get_day_game_stats.py
 python combine.py
 git add .
-git commit -m "${$(date -u -I)} Data Update"
+git commit -m "${current_date} Data Update"
+git push
