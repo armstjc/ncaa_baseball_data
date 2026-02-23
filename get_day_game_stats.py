b841160590f95a843d10c63110c114241386dffe
Joseph Armstrong <70788681+armstjc@users.noreply.github.com>
2026-02-23 07:43:03 -0500

    2026-02-23 Data Update

--- get_day_game_stats.py
+++ get_day_game_stats.py
@@ -101,7 +101,7 @@ def main():
     """ """
     print(datetime.now())
     now_datetime = datetime.now() - timedelta(days=0.5)
-    later_datetime = datetime.now() - timedelta(days=1)
+    later_datetime = datetime.now() - timedelta(days=4)
 
     # now_datetime = datetime(
     #     year=2024,
--- main.sh
+++ main.sh
@@ -4,5 +4,5 @@ git pull
 python get_day_game_stats.py
 python combine.py
 git add .
-git commit -m "${current_date} Data Update"
+git commit -m "${$(date)} Data Update"
 git push
